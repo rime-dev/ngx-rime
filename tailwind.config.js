8bb975bb4871312f99ebf5fa93096a2c0847a383
Carlos Bayarri Cebrecos <e_cbayar@mercadona.com>
2021-08-13 20:32:22 +0200

    feat: add storybook, tailwind and material

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+const { guessProductionMode } = require("@ngneat/tailwind");
+
+module.exports = {
+    prefix: '',
+    purge: {
+      enabled: guessProductionMode(),
+      content: [
+        './apps/**/*.{html,ts}',
+        './libs/**/*.{html,ts}',
+      ]
+    },
+    darkMode: 'class', // or 'media' or 'class'
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+};
