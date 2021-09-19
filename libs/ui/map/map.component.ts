28e50a71d8f6c76ed62f5efb9f94b4ef1915446f
Carlos Bayarri Cebrecos <e_cbayar@mercadona.com>
2021-09-19 13:26:22 +0200

    feat: create component

---
+++ libs/ui/map/map.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'rng-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.scss'],
+})
+export class MapComponent {
+  constructor() {}
+}
