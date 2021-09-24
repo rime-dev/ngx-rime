db9f67ed0d1fc36d94d50ffb67c0a012adacd45b
Carlos Bayarri Cebrecos <e_cbayar@mercadona.com>
2021-09-24 20:19:12 +0200

    fix: change mock implementation

--- apps/demos/quiz-app/src/app/app.module.ts
+++ apps/demos/quiz-app/src/app/app.module.ts
@@ -1,13 +1,50 @@
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
-import {QuizModule} from '@rng/ui/quiz';
+import {Question, QuizModule} from '@rng/ui/quiz';
 import {AppComponent} from './app.component';
 import {Mock} from '@rng/util/mock';
-const users: any[] = [];
+import {HttpClientModule} from '@angular/common/http';
+
+const questions: Question[] = [
+  {
+    title: 'Question 1?',
+    options: [
+      {
+        text: 'Option A',
+        state: true,
+      },
+      {
+        text: 'Option B',
+        state: false,
+      },
+      {
+        text: 'Option C',
+        state: false,
+      },
+    ],
+  },
+  {
+    title: 'Question 2?',
+    options: [
+      {
+        text: 'Option A',
+        state: false,
+      },
+      {
+        text: 'Option B',
+        state: true,
+      },
+      {
+        text: 'Option C',
+        state: false,
+      },
+    ],
+  },
+];
 @NgModule({
   declarations: [AppComponent],
-  imports: [CommonModule, QuizModule],
-  providers: [Mock.provider(users)],
+  imports: [CommonModule, HttpClientModule, QuizModule],
+  providers: [Mock.provider(questions)],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
