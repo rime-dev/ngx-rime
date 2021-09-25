b3cb496b4c6ed1acf60ec0127224ca49c68bb2f4
Carlos Bayarri Cebrecos <e_cbayar@mercadona.com>
2021-09-25 17:55:16 +0200

    refactor: change getter questions on quiz app

--- apps/demos/quiz-app/src/app/app.module.ts
+++ apps/demos/quiz-app/src/app/app.module.ts
@@ -1,46 +1,11 @@
 import {CommonModule} from '@angular/common';
-import {NgModule} from '@angular/core';
-import {Question, QuizModule} from '@rng/ui/quiz';
-import {AppComponent} from './app.component';
-import {Mock} from '@rng/util/mock';
 import {HttpClientModule} from '@angular/common/http';
+import {NgModule} from '@angular/core';
+import {QuizModule} from '@rng/ui/quiz';
+import {Mock} from '@rng/util/mock';
+import questions from '../assets/questions.json';
+import {AppComponent} from './app.component';
 
-const questions: Question[] = [
-  {
-    title: 'Question 1?',
-    options: [
-      {
-        text: 'Option A',
-        state: true,
-      },
-      {
-        text: 'Option B',
-        state: false,
-      },
-      {
-        text: 'Option C',
-        state: false,
-      },
-    ],
-  },
-  {
-    title: 'Question 2?',
-    options: [
-      {
-        text: 'Option A',
-        state: false,
-      },
-      {
-        text: 'Option B',
-        state: true,
-      },
-      {
-        text: 'Option C',
-        state: false,
-      },
-    ],
-  },
-];
 @NgModule({
   declarations: [AppComponent],
   imports: [CommonModule, HttpClientModule, QuizModule],
