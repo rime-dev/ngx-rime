40a391302fa3a0376a047fe8b53bdef85caa67d3
Carlos Bayarri Cebrecos <e_cbayar@mercadona.com>
2021-10-02 19:48:29 +0200

    feat: add toolbar

--- apps/demos/quiz-app/src/app/app.module.ts
+++ apps/demos/quiz-app/src/app/app.module.ts
@@ -1,8 +1,8 @@
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-import {AppRoutingModule} from './app-routing.module';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 
 @NgModule({
--- apps/demos/quiz-app/src/app/modules/home/home.module.ts
+++ apps/demos/quiz-app/src/app/modules/home/home.module.ts
@@ -2,12 +2,13 @@ import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
 import {TargetCardComponent} from './components/target-card/target-card.component';
 import {HomeRoutingModule} from './home-routing.module';
 import {HomeComponent} from './home.component';
 
 @NgModule({
   declarations: [HomeComponent, TargetCardComponent],
-  imports: [CommonModule, HomeRoutingModule, MatButtonModule, MatCardModule],
+  imports: [CommonModule, HomeRoutingModule, MatButtonModule, MatToolbarModule, MatCardModule],
 })
 export class HomeModule {}
