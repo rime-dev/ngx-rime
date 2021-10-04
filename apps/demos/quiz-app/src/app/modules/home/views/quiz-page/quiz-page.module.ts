562de2202a307eee5e7e2cf795eee09c19e82197
Carlos Bayarri Cebrecos <e_cbayar@mercadona.com>
2021-10-04 18:36:58 +0200

    fix: question assets

--- apps/demos/quiz-app/src/app/modules/home/views/quiz-page/quiz-page.module.ts
+++ apps/demos/quiz-app/src/app/modules/home/views/quiz-page/quiz-page.module.ts
@@ -5,11 +5,11 @@ import {QuizModule} from '@rng/ui/quiz';
 import {Mock} from '@rng/data-access/mock';
 import {QuizPageRoutingModule} from './quiz-page-routing.module';
 import {QuizPageComponent} from './quiz-page.component';
-import questions from '../../../../../assets/questions.json';
+import {angular_questions} from 'apps/demos/quiz-app/src/assets/questions';
 
 @NgModule({
   declarations: [QuizPageComponent],
   imports: [CommonModule, HttpClientModule, QuizModule, QuizPageRoutingModule],
-  providers: [Mock.provider(questions)],
+  providers: [Mock.provider(angular_questions)],
 })
 export class QuizPageModule {}
