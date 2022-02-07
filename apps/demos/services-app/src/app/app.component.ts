ea02ae33c8ea3b661cf36337790e9dd3e65a6607
Carlos Bayarri Cebrecos <e_cbayar@mercadona.com>
2022-02-07 09:44:34 +0100

    feat: add changes

--- apps/demos/services-app/src/app/app.component.ts
+++ apps/demos/services-app/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'rng-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'rng-app';
+
+  ngOnInit(): void {
+    if (
+      localStorage.getItem('theme') === 'dark' ||
+      (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
+    ) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }
 }
