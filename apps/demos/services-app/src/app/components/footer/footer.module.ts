11b1131749e236e2bf1c124a210a7acdb49ad276
Carlos Bayarri Cebrecos <e_cbayar@mercadona.com>
2022-03-28 18:13:41 +0200

    feat: add footer

--- apps/demos/services-app/src/app/components/footer/footer.module.ts
+++ apps/demos/services-app/src/app/components/footer/footer.module.ts
@@ -3,12 +3,21 @@ import {NgModule} from '@angular/core';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatIconModule} from '@angular/material/icon';
 import {RouterModule} from '@angular/router';
-import {TranslocoModule} from '@ngneat/transloco';
+import {LangDefinition, Translation, TranslocoModule, TranslocoService} from '@ngneat/transloco';
 import {FooterComponent} from './footer.component';
+import i18n from './i18n/i18n';
 
 @NgModule({
   declarations: [FooterComponent],
   exports: [FooterComponent],
   imports: [CommonModule, TranslocoModule, RouterModule, MatDividerModule, MatIconModule],
 })
-export class FooterModule {}
+export class FooterModule {
+  constructor(translocoService: TranslocoService) {
+    translocoService.getAvailableLangs().forEach((lang) => {
+      const language: string = (lang as LangDefinition).id || (lang as string);
+      const translation = (i18n as Record<string, Translation>)[language];
+      translocoService.setTranslation(translation, 'footer/' + language);
+    });
+  }
+}
---
+++ apps/demos/services-app/src/app/components/footer/i18n/i18n.ts
@@ -0,0 +1,31 @@
+const translate = {
+  es: {
+    getConnectedWithUsOnSocialNetworks: 'Conéctate con nosotros en las redes sociales',
+    description:
+      'Encuentra lo que necesites para tu hogar. Descubre nuestros servicios de reformas, búsqueda de inmuebles, gestiones legales...',
+    products: 'Productos',
+    realStateWebsite: 'Portal inmobiliario',
+    servicesWebsite: 'Portal servicios',
+    realStatePlatform: 'Plataforma inmobiliaria',
+    servicesPlatform: 'Plataforma servicios',
+    usefulLinks: 'Enlaces de utilidad',
+    home: 'Inicio',
+    pricing: 'Precios',
+    contact: 'Contacto',
+  },
+  en: {
+    getConnectedWithUsOnSocialNetworks: 'Get connected with us on social networks',
+    description:
+      'Find what you need for your home. Discover our reform services, property search, legal procedures...',
+    products: 'Products',
+    realStateWebsite: 'Real state website',
+    servicesWebsite: 'Services website',
+    realStatePlatform: 'Real state platform',
+    servicesPlatform: 'Services platform',
+    usefulLinks: 'Useful links',
+    home: 'Home',
+    pricing: 'Pricing',
+    contact: 'Contact',
+  },
+};
+export default translate;
