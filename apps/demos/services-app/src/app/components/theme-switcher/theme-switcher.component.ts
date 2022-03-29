309682fea435424561c2a7f0063104420a9e8381
Carlos Bayarri Cebrecos <e_cbayar@mercadona.com>
2022-03-29 17:06:47 +0200

    feat: add change in theme switcher

--- apps/demos/services-app/src/app/components/theme-switcher/theme-switcher.component.ts
+++ apps/demos/services-app/src/app/components/theme-switcher/theme-switcher.component.ts
@@ -7,48 +7,43 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./theme-switcher.component.scss'],
 })
 export class ThemeSwitcherComponent implements OnInit {
+  public isDark = false;
   constructor(private overlay: OverlayContainer) {}
+
   ngOnInit(): void {
-    const themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
-    const themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
     if (
       localStorage.getItem('theme') === 'dark' ||
       (!('theme' in localStorage) &&
         window.matchMedia &&
         window.matchMedia('(prefers-color-scheme: dark)').matches)
     ) {
-      themeToggleDarkIcon?.classList.remove('hidden');
+      this.isDark = true;
     } else {
-      themeToggleLightIcon?.classList.remove('hidden');
+      this.isDark = false;
     }
-    const themeToggleBtn = document.getElementById('theme-toggle');
-
-    themeToggleBtn?.addEventListener('click', () => {
-      themeToggleDarkIcon?.classList.toggle('hidden');
-      themeToggleLightIcon?.classList.toggle('hidden');
-      if (localStorage.getItem('theme')) {
-        if (localStorage.getItem('theme') === 'dark') {
-          document.documentElement.classList.remove('dark');
-          this.overlay.getContainerElement().classList.remove('dark');
-
-          localStorage.setItem('theme', 'light');
-        } else {
-          document.documentElement.classList.add('dark');
-          this.overlay.getContainerElement().classList.add('dark');
-
-          localStorage.setItem('theme', 'dark');
-        }
+  }
+  changeTheme() {
+    if (localStorage.getItem('theme')) {
+      if (localStorage.getItem('theme') === 'dark') {
+        document.documentElement.classList.remove('dark');
+        this.overlay.getContainerElement().classList.remove('dark');
+        localStorage.setItem('theme', 'light');
       } else {
-        if (document.documentElement.classList.contains('dark')) {
-          document.documentElement.classList.remove('dark');
-          this.overlay.getContainerElement().classList.remove('dark');
-          localStorage.setItem('theme', 'light');
-        } else {
-          document.documentElement.classList.add('dark');
-          this.overlay.getContainerElement().classList.add('dark');
-          localStorage.setItem('theme', 'dark');
-        }
+        document.documentElement.classList.add('dark');
+        this.overlay.getContainerElement().classList.add('dark');
+        localStorage.setItem('theme', 'dark');
       }
-    });
+    } else {
+      if (document.documentElement.classList.contains('dark')) {
+        document.documentElement.classList.remove('dark');
+        this.overlay.getContainerElement().classList.remove('dark');
+        localStorage.setItem('theme', 'light');
+      } else {
+        document.documentElement.classList.add('dark');
+        this.overlay.getContainerElement().classList.add('dark');
+        localStorage.setItem('theme', 'dark');
+      }
+    }
+    this.isDark = !this.isDark;
   }
 }
