31ef2871e530a0847f18ea3296ad1bb28491b773
Carlos Bayarri Cebrecos <e_cbayar@mercadona.com>
2022-04-03 18:48:43 +0200

    fix: add export of model

--- libs/data-access/base/index.ts
+++ libs/data-access/base/index.ts
@@ -2,3 +2,4 @@ export * from './base.module';
 export * from './services/data/data.service';
 export * from './decorators/data-filter.decorator';
 export * from './operators/data-filter.operator';
+export * from './models/base.model';
