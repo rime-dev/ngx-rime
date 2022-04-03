c49d5e7e54dcd4cafb1baf45d8e55bddd54ec9e6
Carlos Bayarri Cebrecos <e_cbayar@mercadona.com>
2022-04-03 18:45:00 +0200

    fix: change index and exports

--- libs/data-access/base/index.ts
+++ libs/data-access/base/index.ts
@@ -1,4 +1,4 @@
 export * from './base.module';
 export * from './services/data/data.service';
-export * from './decorators';
-export * from './operators';
+export * from './decorators/data-filter.decorator';
+export * from './operators/data-filter.operator';
